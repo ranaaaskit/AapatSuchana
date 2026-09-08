4d5913e1894df4b8173e18a4b5bcb14e880bcb3d
ranaaaskit <ranaaaskit@gmail.com>
2026-09-08 18:33:31 +0545

    Update backend configuration and bytecode

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -58,7 +58,10 @@ USE_TZ = True
 STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ALLOWED_ORIGINS = ['http://localhost:5173']
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:5173',
+    'http://127.0.0.1:5173',
+]
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
