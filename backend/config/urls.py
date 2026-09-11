387c4d35c94963713def840e5c3f5be31aedc96e
ranaaaskit <ranaaaskit@gmail.com>
2026-09-11 16:55:12 +0545

    Adds API root endpoint for health checks

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.http import JsonResponse
 from django.urls import include, path
 from rest_framework_simplejwt.views import TokenRefreshView
 from incidents.serializers import EmailTokenObtainPairSerializer
@@ -8,8 +9,14 @@ from rest_framework_simplejwt.views import TokenObtainPairView
 class EmailTokenObtainPairView(TokenObtainPairView):
     serializer_class = EmailTokenObtainPairSerializer
 
+
+def api_root(request):
+    return JsonResponse({'status': 'ok', 'message': 'AapatSuchana API is running'})
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', api_root, name='api-root'),
     path('api/auth/token/', EmailTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/', include('incidents.urls')),
